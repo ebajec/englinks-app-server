d5ea2fa58b69e337ffc25f46e8b46ae8a8e73b93
Elijah Bajec Dickinson <43395424+ebajec@users.noreply.github.com>
2023-04-01 18:18:19 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ def test():
 @app.route('/recieve_tutor_request', methods = ['POST'])
 def update_file():
 
-    requestData = (str)(request.data.decode('utf-8'))
-    data = json.loads(requestData)
+    body = (str)(request.data.decode('utf-8'))
+    data = json.loads(body)
     filepath = 'tutor_requests/' +data['user'] +'/'
 
     if(not os.path.isdir(filepath)): 
@@ -30,14 +30,36 @@ def update_file():
             request_num += 1
         else:
             with open(filepath + filename, 'x') as f:
-                f.write(requestData)   
+                f.write(body)   
 
             return 'Tutor request recieved!'
 
+@app.route('/read_tutor_requests',methods = ['POST'])
+def get_tutor_requests_data():
+
+    body = (str)(request.data.decode('utf-8'))
+    data = json.loads(body)
+
+    dir = 'tutor_requests/' + data['username'] +'/'
+
+    requestList = []
+
+    if os.path.isdir(dir):
+        for filename in os.listdir(dir):
+            fullpath = os.path.join(dir, filename)
+
+            if os.path.isfile(fullpath):
+                f = open(fullpath,'r')
+                requestList.append(f.read())
+
+    return json.dumps(requestList)
+    
+
+
 @app.route('/login',methods = ['POST'])
 def login():
-    requestData = (str)(request.data.decode('utf-8'))
-    data = json.loads(requestData)
+    body = (str)(request.data.decode('utf-8'))
+    data = json.loads(body)
 
     username = data['username']
     password = data['password']
@@ -48,13 +70,14 @@ def login():
           
 @app.route('/event_data/<string:file>')
 def get_calendar_data(file):
-    requestData = (str)(request.data.decode('utf-8'))
 
     f = open(file,'r')
     return f.read()
     
 
+
+
 if __name__ ==  '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.2.92', port=5000)
 
 
