5f690f943d7f99ae1903f90475ce6149a0310c53
Eli <20ebd1@queensu.ca>
2023-04-06 10:41:40 -0400

    removed unused import

--- bounce_scraper.py
+++ bounce_scraper.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import json
-from cryptography.fernet import Fernet
 
 class PushStack:
     def __init__(self,size):
